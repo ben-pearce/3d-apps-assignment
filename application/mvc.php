a07f4a7fb45ec77e58f4af0b8c635ca686a607c1
Ben <ben@solero.me>
2021-03-23 10:28:33 +0000

    Add support for fancy URLs using .htaccess & modrewrite

--- application/mvc.php
+++ application/mvc.php
@@ -5,7 +5,10 @@ require 'model/model.php';
 require 'controller/controller.php';
 
 $pageURI = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-$pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 10);
+$pageURI = trim($pageURI, '/');
+if(substr($pageURI, 0, strlen('index.php')) === 'index.php') {
+  $pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 10);
+}
 
 if(!$pageURI) {
   new Controller('home');
