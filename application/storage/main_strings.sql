b0d9a7de1606e3f8db0f31a1c4cdca7c23daae76
Ben <ben@solero.me>
2021-03-25 14:29:44 +0000

    Database string data script

---
+++ application/storage/main_strings.sql
@@ -0,0 +1,8 @@
+INSERT INTO strings (key, value) VALUES ('site.title', 'Coca Cola Journey - Refreshing the world, one story at a time');
+INSERT INTO strings (key, value) VALUES ('home.title', 'Coca Cola Great Britain');
+INSERT INTO strings (key, value) VALUES ('home.subtitle', 'Founded in 1886 Dr John S Pemberton');
+INSERT INTO strings (key, value) VALUES ('home.description', 'The Coca Cola Company is the world''s leading manufacturer');
+INSERT INTO strings (key, value) VALUES ('model.gallery.title', '3D Images');
+INSERT INTO strings (key, value) VALUES ('model.gallery.description', 'These 3D images of the Coke can, Sprite bottle and Dr Pepper cup are rendered in 3DS Max 2019');
+INSERT INTO strings (key, value) VALUES ('model.camera.title', 'Camera Views');
+INSERT INTO strings (key, value) VALUES ('model.camera.subtitle', 'These buttons select a range of X3D model viewpoints');
