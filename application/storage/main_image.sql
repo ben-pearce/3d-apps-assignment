bd8d1e0285ccfccbb3ead7c6a79afab451d5e5c8
Ben <ben@solero.me>
2021-03-25 14:29:22 +0000

    Database image data script

---
+++ application/storage/main_image.sql
@@ -0,0 +1,3 @@
+INSERT INTO image (id, brand, name, description, image_src_path) VALUES (1, 'coke', 'Coke can', 'Coke can rendered by 3DS Max', '/assets/images/gallery/coke_1.jpg');
+INSERT INTO image (id, brand, name, description, image_src_path) VALUES (2, 'sprite', 'Sprite bottle', 'Sprite bottle rendered by 3DS Max', '/assets/images/gallery/sprite_1.jpg');
+INSERT INTO image (id, brand, name, description, image_src_path) VALUES (3, 'pepper', 'Dr Pepper cup', 'Dr Pepper cup rendered by 3DS Max', '/assets/images/gallery/pepper_1.jpg');
