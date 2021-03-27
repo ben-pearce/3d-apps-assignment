b3e1d8ffb833e0187bf5da409af6e5f745cd98eb
Ben <ben@solero.me>
2021-03-27 15:18:45 +0000

    Get correct page URI for sussex webspace

--- application/mvc.php
+++ application/mvc.php
@@ -5,7 +5,8 @@ require 'model/model.php';
 require 'controller/controller.php';
 
 $pageURI = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-$pageURI = trim($pageURI, '/');
+$pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 10);
+
 if(substr($pageURI, 0, strlen('index.php')) === 'index.php') {
   $pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 10);
 }
