f633a71af807d8d73b14b7b5ae309e7332c5fe75
Ben <ben@solero.me>
2021-03-27 15:19:05 +0000

    Documentation of MVC entry-point script

--- application/mvc.php
+++ application/mvc.php
@@ -1,9 +1,26 @@
 <?php
 
+/**
+ * Entrypoint for MVC application.
+ */
+
 require 'view/load.php';
 require 'model/model.php';
 require 'controller/controller.php';
 
+/**
+ * Find the endpoint which has been requested by the user.
+ * 
+ * If they have accessed the site via index.php, the endpoint
+ * will be everything in the url that follows that i.e.
+ * 
+ * index.php/aboutView -> aboutView
+ * 
+ * If they have accessed without index.php it will just be
+ * everything that follows the base url i.e.
+ * 
+ * /aboutView -> aboutView
+ */
 $pageURI = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 10);
 
@@ -11,6 +28,12 @@ if(substr($pageURI, 0, strlen('index.php')) === 'index.php') {
   $pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 10);
 }
 
+/**
+ * Invoke the controller with the appropriate endpoint.
+ * 
+ * If the endpoint is undefined then invoke the default
+ * 'home' endpoint.
+ */
 if(!$pageURI) {
   new Controller('home');
 } else {
