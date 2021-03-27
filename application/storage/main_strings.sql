817f4a68f22dd8d3f3768297987b672912936140
Ben <ben@solero.me>
2021-03-27 15:11:23 +0000

    Update strings table install script

--- application/storage/main_strings.sql
+++ application/storage/main_strings.sql
@@ -1,8 +1,13 @@
-INSERT INTO strings (key, value) VALUES ('site.title', 'Coca Cola Journey - Refreshing the world, one story at a time');
 INSERT INTO strings (key, value) VALUES ('home.title', 'Coca Cola Great Britain');
 INSERT INTO strings (key, value) VALUES ('home.subtitle', 'Founded in 1886 Dr John S Pemberton');
-INSERT INTO strings (key, value) VALUES ('home.description', 'The Coca Cola Company is the world''s leading manufacturer');
+INSERT INTO strings (key, value) VALUES ('home.description', 'The Coca Cola Company is the world''s leading manufacturer, marketer and distributor of non-alchoholic beverage concentrates and syrups, and produces nearly 400 brands.');
 INSERT INTO strings (key, value) VALUES ('model.gallery.title', '3D Images');
-INSERT INTO strings (key, value) VALUES ('model.gallery.description', 'These 3D images of the Coke can, Sprite bottle and Dr Pepper cup are rendered in 3DS Max 2019');
+INSERT INTO strings (key, value) VALUES ('model.gallery.description', 'These 3D images of the are rendered in 3DS Max and Mental Ray.');
 INSERT INTO strings (key, value) VALUES ('model.camera.title', 'Camera Views');
-INSERT INTO strings (key, value) VALUES ('model.camera.subtitle', 'These buttons select a range of X3D model viewpoints');
+INSERT INTO strings (key, value) VALUES ('model.camera.subtitle', 'These buttons select a range of X3D model viewpoints.');
+INSERT INTO strings (key, value) VALUES ('site.tagline', 'Refreshing the world, one story at a time');
+INSERT INTO strings (key, value) VALUES ('model.animation.title', 'Animation Options');
+INSERT INTO strings (key, value) VALUES ('model.animation.subtitle', 'These buttons select a range of X3D animation options.');
+INSERT INTO strings (key, value) VALUES ('model.render.title', 'Render and Lighting Options');
+INSERT INTO strings (key, value) VALUES ('model.render.subtitle', 'These buttons select a range of X3D rendering options.');
+INSERT INTO strings (key, value) VALUES ('site.title', 'Coca Cola Journey');
