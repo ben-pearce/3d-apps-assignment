654d8ec22a87a4dfc11b1d6aead2ccb5a9543caf
Ben <ben@solero.me>
2021-03-27 15:32:00 +0000

    Simple message template view file

---
+++ application/view/messageView.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Message</title>
+  </head>
+  <body>
+    <h2>Message</h2>
+    <hr>
+    <p><?= $data ?></p>
+    <hr>
+    <a href="/">Back to homepage</a>
+  </body>
+</html>
