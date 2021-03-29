db8120af733c53377932460ef33428405424598a
Ben <ben@solero.me>
2021-03-29 17:12:25 +0100

    Fix appletiser texture path

--- application/storage/main_texture.sql
+++ application/storage/main_texture.sql
@@ -3,7 +3,7 @@ INSERT INTO texture (id, model, name, texture_src_path) VALUES (2, 2, 'Sprite', 
 INSERT INTO texture (id, model, name, texture_src_path) VALUES (3, 3, 'Dr. Pepper', 'textures/pepper.jpg');
 INSERT INTO texture (id, model, name, texture_src_path) VALUES (4, 4, 'Costa', 'textures/costa.jpg');
 INSERT INTO texture (id, model, name, texture_src_path) VALUES (5, 5, 'Fanta Orange', 'textures/fanta.jpg');
-INSERT INTO texture (id, model, name, texture_src_path) VALUES (6, 6, 'Appletizer', 'textures/appletizer.jpg');
+INSERT INTO texture (id, model, name, texture_src_path) VALUES (6, 6, 'Appletizer', 'textures/appletizer.png');
 INSERT INTO texture (id, model, name, texture_src_path) VALUES (7, 1, 'Coke Zero', 'textures/coke_zero.jpg');
 INSERT INTO texture (id, model, name, texture_src_path) VALUES (8, 1, 'Coke Diet', 'textures/coke_diet.jpg');
 INSERT INTO texture (id, model, name, texture_src_path) VALUES (9, 5, 'Fanta Lime', 'textures/fanta_lime.jpg');
