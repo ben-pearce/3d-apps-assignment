299517d106d8eef87c31639ed2b6f6699df5b232
Ben <ben@solero.me>
2021-03-29 13:27:21 +0100

    Add textures data SQL script

---
+++ application/storage/main_texture.sql
@@ -0,0 +1,9 @@
+INSERT INTO texture (id, model, name, texture_src_path) VALUES (1, 1, 'Coke Regular', 'textures/coke.jpg');
+INSERT INTO texture (id, model, name, texture_src_path) VALUES (2, 2, 'Sprite', 'textures/sprite.jpg');
+INSERT INTO texture (id, model, name, texture_src_path) VALUES (3, 3, 'Dr. Pepper', 'textures/pepper.jpg');
+INSERT INTO texture (id, model, name, texture_src_path) VALUES (4, 4, 'Costa', 'textures/costa.jpg');
+INSERT INTO texture (id, model, name, texture_src_path) VALUES (5, 5, 'Fanta Orange', 'textures/fanta.jpg');
+INSERT INTO texture (id, model, name, texture_src_path) VALUES (6, 6, 'Appletizer', 'textures/appletizer.jpg');
+INSERT INTO texture (id, model, name, texture_src_path) VALUES (7, 1, 'Coke Zero', 'textures/coke_zero.jpg');
+INSERT INTO texture (id, model, name, texture_src_path) VALUES (8, 1, 'Coke Diet', 'textures/coke_diet.jpg');
+INSERT INTO texture (id, model, name, texture_src_path) VALUES (9, 5, 'Fanta Lime', 'textures/fanta_lime.jpg');
